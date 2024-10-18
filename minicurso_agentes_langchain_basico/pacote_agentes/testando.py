9dc89b664bea488c183fc7c9334065d2602aa5b5
Pablo De Chiaro Rosa <pablo.chiaro.rosa@gmail.com>
2024-10-18 19:16:01 -0300

    branch do curso

--- minicurso_agentes_langchain_basico/pacote_agentes/testando.py
+++
@@ -1,6 +0,0 @@
-def oi() -> None:
-    print("Oi, eu sou um agente!")
-
-
-def tchau() -> None:
-    print("Até mais")
--- minicurso_agentes_langchain_basico/testa_pacote.py
+++
@@ -1,6 +0,0 @@
-from .pacote_agentes import *
-
-
-def main() -> None:
-    oi()
-    tchau()  # a função tchau não está definida no pacote como *
